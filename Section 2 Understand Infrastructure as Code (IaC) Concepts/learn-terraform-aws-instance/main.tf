ff1cb9b7170f03a2fbb62ad192a6846997f2973b
noweder <hmnoweder@gmail.com>
2021-12-21 23:55:54 +0200

    initial commit

---
+++ Section 2 Understand Infrastructure as Code (IaC) Concepts/learn-terraform-aws-instance/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0ed9277fb7eb570c9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
