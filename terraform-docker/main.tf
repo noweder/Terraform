2745ccb7033a31e06095d64af138fd171fcabdea
Hamza Noweder <hmnoweder@gmail.com>
2022-05-21 21:02:35 +0000

    created image module

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,21 +1,11 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.15.0"
-    }
-  }
-}
-
-provider "docker" {}
-
 resource "null_resource" "dockervol" {
   provisioner "local-exec" {
   command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
   }
 }
-resource "docker_image" "nodered_image" {
-  name = var.image[terraform.workspace]
+
+module "image" {
+  source = "./image"
 }
 
 resource "random_string" "random" {
@@ -30,7 +20,7 @@ resource "random_string" "random" {
 resource "docker_container" "nodered_container" {
   count = local.container_count
   name  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image = docker_image.nodered_image.latest
+  image = module.image.image_out
   ports {
     internal = var.int_port
     external = var.ext_port[terraform.workspace][count.index]
