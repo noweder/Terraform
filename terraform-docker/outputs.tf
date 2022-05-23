9e46c7c1cf60e73179a7f2a72bf2a38a8dce053f
Hamza Noweder <hmnoweder@gmail.com>
2022-05-23 16:50:49 +0000

    updated root and module outputs

--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -1,9 +1,9 @@
-# output "container-name" {
-#     value = docker_container.nodered_container[*].name
-#     description = "The name of the container"
-# }
+output "container-name" {
+    value = module.container[*].container-name
+    description = "The name of the container"
+}
 
-# output "IP-Address" {
-#     value = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address], i.ports[*]["external"])]
-#     description = "The IP address and external port of the container"
-# }
+output "IP-Address" {
+    value = flatten(module.container[*].IP-Address)
+    description = "The IP address and external port of the container"
+}
