3f5ca2e3d6c3e65fd6e87e5f4cd7e6e0165b73a1
Hamza Noweder <hmnoweder@gmail.com>
2022-05-23 15:14:45 +0000

    added explicit dependency in nodered_container

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "dockervol" {
   provisioner "local-exec" {
-  command = "sleep 60 && mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+  command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
   }
 }
 
@@ -19,8 +19,9 @@ resource "random_string" "random" {
 
 
 resource "docker_container" "nodered_container" {
+  depends_on = [null_resource.dockervol]
   count = local.container_count
-  name  = join("-", ["nodered", terraform.workspace, null_resource.dockervol.id, random_string.random[count.index].result])
+  name  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
   image = module.image.image_out
   ports {
     internal = var.int_port
