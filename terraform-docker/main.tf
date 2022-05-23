19cb13fe0aa3a2c8bdd150a332990ea80b7e1f6f
Hamza Noweder <hmnoweder@gmail.com>
2022-05-23 15:08:45 +0000

    removed one port from terraform.tfvars and added implicit dependency between dockervol and nodered_container

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "dockervol" {
   provisioner "local-exec" {
-  command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+  command = "sleep 60 && mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
   }
 }
 
@@ -20,7 +20,7 @@ resource "random_string" "random" {
 
 resource "docker_container" "nodered_container" {
   count = local.container_count
-  name  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
+  name  = join("-", ["nodered", terraform.workspace, null_resource.dockervol.id, random_string.random[count.index].result])
   image = module.image.image_out
   ports {
     internal = var.int_port
