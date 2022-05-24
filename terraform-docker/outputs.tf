dc55bd6377dbdd57dee4f3ccd5f9dd9a993b357f
Hamza Noweder <hmnoweder@gmail.com>
2022-05-24 21:10:01 +0000

    Deploying Containers with for_each

--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -1,9 +1,9 @@
-output "container-name" {
-  value       = module.container[*].container-name
-  description = "The name of the container"
-}
+# output "container-name" {
+#   value       = module.container[*].container-name
+#   description = "The name of the container"
+# }
 
-output "IP-Address" {
-  value       = flatten(module.container[*].IP-Address)
-  description = "The IP address and external port of the container"
-}
+# output "IP-Address" {
+#   value       = flatten(module.container[*].IP-Address)
+#   description = "The IP address and external port of the container"
+# }
