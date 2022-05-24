60e70083d4e346e124d72559685a7978a74a9cd4
Hamza Noweder <hmnoweder@gmail.com>
2022-05-24 15:02:02 +0000

    added influxdb_image

--- terraform-docker/container/variables.tf
+++ terraform-docker/container/variables.tf
@@ -1,6 +1,5 @@
 variable "ext_port_in" {}
 variable "int_port_in" {}
 variable "container_path_in" {}
-variable "host_path_in" {}
 variable "name_in" {}
 variable "image_in" {}
--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,27 +1,29 @@
+module "nodered_image" {
+  source   = "./image"
+  image_in = var.image["nodered"][terraform.workspace]
+}
 
-module "image" {
-  source = "./image"
-  image_in = var.image[terraform.workspace]
+module "influxdb_image" {
+  source   = "./image"
+  image_in = var.image["influxdb"][terraform.workspace]
 }
 
 resource "random_string" "random" {
-  count = local.container_count
-  length = 4
+  count   = local.container_count
+  length  = 4
   special = false
-  upper = false
-  
+  upper   = false
+
 }
 
-
 module "container" {
-  source = "./container"
-  count = local.container_count
-  name_in  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image_in = module.image.image_out
-  int_port_in = var.int_port
-  ext_port_in = var.ext_port[terraform.workspace][count.index]
+  source            = "./container"
+  count             = local.container_count
+  name_in           = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
+  image_in          = module.nodered_image.image_out
+  int_port_in       = var.int_port
+  ext_port_in       = var.ext_port[terraform.workspace][count.index]
   container_path_in = "/data"
-  host_path_in = "${path.cwd}/noderedvol"
 }
 
 
