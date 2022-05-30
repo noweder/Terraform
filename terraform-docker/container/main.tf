b72a80274ab69ed22920d1a5d1466b3a6e1de315
Hamza Noweder <hmnoweder@gmail.com>
2022-05-30 20:03:35 +0000

    added nested volume module

--- terraform-docker/container/main.tf
+++ terraform-docker/container/main.tf
@@ -3,7 +3,6 @@ resource "random_string" "random" {
   length  = 4
   special = false
   upper   = false
-
 }
 
 resource "docker_container" "app_container" {
@@ -18,33 +17,22 @@ resource "docker_container" "app_container" {
     for_each = var.volumes_in
     content {
       container_path = volumes.value["container_path_each"]
-      volume_name    = docker_volume.container_volume[volumes.key].name
+      volume_name    = module.volume[count.index].volume_output[volumes.key]
     }
   }
   provisioner "local-exec" {
-    command = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"] : x])} >> ${path.cwd}/containers.txt"
+    command = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"] : x])} >> test.txt"
   }
   provisioner "local-exec" {
     when    = destroy
-    command = "rm -f ${path.cwd}/containers.txt"
-  }
-
-}
-
-resource "docker_volume" "container_volume" {
-  count = length(var.volumes_in)
-  name  = "${var.name_in}-${count.index}-volume"
-  lifecycle {
-    prevent_destroy = false
-  }
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "mkdir ${path.cwd}/../backup/"
-    on_failure = continue
-  }
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
-    on_failure = fail
+    command = "rm -f test.txt"
   }
 }
+
+module "volume" {
+  source       = "./volume"
+  count        = var.count_in
+  volume_count = length(var.volumes_in)
+  volume_name  = "${var.name_in}-${terraform.workspace}-${random_string.random[count.index].result}-volume"
+}
+
