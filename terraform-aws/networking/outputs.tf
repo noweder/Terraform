d3f02cfd58dcf4dcf274cf5698aba670beffd6f5
Hamza Noweder <hmnoweder@gmail.com>
2022-06-03 21:06:38 +0000

    added database module

--- terraform-aws/networking/outputs.tf
+++ terraform-aws/networking/outputs.tf
@@ -3,3 +3,11 @@
 output "vpc_id" {
   value = aws_vpc.noweder_vpc.id
 }
+
+output "db_subnet_group_name" {
+    value = aws_db_subnet_group.noweder_rds_subnetgroup.*.name
+}
+
+output "db_security_group" {
+    value = [aws_security_group.noweder_sg["rds"].id]
+}
