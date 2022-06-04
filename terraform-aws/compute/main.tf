bdc01f3d9a20985a7313eceb787ae9c005f8e088
Hamza Noweder <hmnoweder@gmail.com>
2022-06-04 22:10:38 +0000

    added compute module config

--- terraform-aws/compute/main.tf
+++ terraform-aws/compute/main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "server_ami" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20220411"] # free tier version
   }
 
   #   filter {
@@ -18,4 +18,26 @@ data "aws_ami" "server_ami" {
   #         name   = "virtualization-type"
   #         values = ["hvm"]
   #   }
+}
+
+resource "random_id" "noweder_node_id" {
+  byte_length = 2
+  count       = var.instance_count
+}
+
+resource "aws_instance" "noweder_node" {
+  count         = var.instance_count # 1
+  instance_type = var.instance_type  # t3.micro
+  ami           = data.aws_ami.server_ami.id
+  tags = {
+    Name = "noweder_node-${random_id.noweder_node_id[count.index].dec}"
+  }
+
+  # key_name = ""
+  vpc_security_group_ids = [var.public_sg]
+  subnet_id              = var.public_subnets[count.index]
+  # user_data = ""
+  root_block_device {
+    volume_size = var.vol_size # 10
+  }
 }
