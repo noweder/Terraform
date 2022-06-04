9c4f92e79131b6a14f1139539c45a604a82218c3
Hamza Noweder <hmnoweder@gmail.com>
2022-06-04 10:11:23 +0000

    added target group resouce

--- terraform-aws/loadbalancing/main.tf
+++ terraform-aws/loadbalancing/main.tf
@@ -6,3 +6,16 @@ resource "aws_lb" "noweder-lb" {
   security_groups = [var.public_sg]
   idle_timeout    = 400
 }
+
+resource "aws_lb_target_group" "noweder-tg" {
+  name     = "noweder-lb-tg-${substr(uuid(), 0, 4)}"
+  port     = var.tg_port     #80
+  protocol = var.tg_protocol # "HTTP"
+  vpc_id   = var.vpc_id      #from networking
+  health_check {
+    healthy_threshold   = var.lb_healthy_threshold   #2
+    unhealthy_threshold = var.lb_unhealthy_threshold #2
+    timeout             = var.lb_timeout             #3
+    interval            = var.lb_interval            #30
+  }
+}
