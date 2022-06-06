b478eb0e2dee6eb8b0ca3bd7d20878d761e4a541
Hamza Noweder <hmnoweder@gmail.com>
2022-06-06 20:11:41 +0000

    changed the instances output format

--- terraform-aws/compute/outputs.tf
+++ terraform-aws/compute/outputs.tf
@@ -1,5 +1,9 @@
 # --- compute/outputs.tf ---
 
 output "instance" {
-  value = aws_instance.noweder_node[*]
+    value = aws_instance.noweder_node[*]
+}
+
+output "instance_port" {
+    value = aws_lb_target_group_attachment.noweder_tg_attach[0].port
 }
